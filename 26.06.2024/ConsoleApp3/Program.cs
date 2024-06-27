c6a301928e61e5314583ce685e2c2aadbdc86cb5
hosam-abuelhija <hosam.adnan123@gmail.com>
2024-06-27 20:30:04 +0300

    cs

--- 26.06.2024/ConsoleApp3/Program.cs
+++ 26.06.2024/ConsoleApp3/Program.cs
@@ -10,7 +10,7 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            //task 1
+            ////task 1
 
             //Console.Write("Enter the first number: ");
             //int num1 = Convert.ToInt32(Console.ReadLine());
@@ -19,46 +19,46 @@ namespace Task_2
             //int smaller = (num1 < num2) ? num1 : num2;
             //Console.WriteLine($"The smaller number is: {smaller}");
 
-            //Task 2
+            ////Task 2
             //Console.Write("Enter a number: ");
             //int number = Convert.ToInt32(Console.ReadLine());
             //int sign = Math.Sign(number);
-            //string signMessage = sign == 1 ? "The sign is +" :sign == -1 ? "The sign is -" :"The number is zero";
+            //string signMessage = sign == 1 ? "The sign is +" : sign == -1 ? "The sign is -" : "The number is zero";
             //Console.WriteLine(signMessage);
 
-            //Task 3
+            ////Task 3
             //Console.Write("Enter the first number: ");
-            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //num1 = Convert.ToInt32(Console.ReadLine());
             //Console.Write("Enter the second number: ");
-            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //num2 = Convert.ToInt32(Console.ReadLine());
             //Console.Write("Enter the third number: ");
             //int num3 = Convert.ToInt32(Console.ReadLine());
             //int[] nums = { num1, num2, num3 };
             //Array.Sort(nums);
             //Console.WriteLine($"Sorted numbers: {nums[0]}, {nums[1]}, {nums[2]}");
 
-            //Task 4
+            ////Task 4
             //Console.Write("Enter the first number: ");
-            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //num1 = Convert.ToInt32(Console.ReadLine());
             //Console.Write("Enter the second number: ");
-            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //num2 = Convert.ToInt32(Console.ReadLine());
             //Console.Write("Enter the third number: ");
-            //int num3 = Convert.ToInt32(Console.ReadLine());
+            //num3 = Convert.ToInt32(Console.ReadLine());
             //Console.Write("Enter the fourth number: ");
             //int num4 = Convert.ToInt32(Console.ReadLine());
             //Console.Write("Enter the fifth number: ");
             //int num5 = Convert.ToInt32(Console.ReadLine());
-            //int[] nums = { num1, num2, num3, num4, num5 };
+            //nums = new int[] { num1, num2, num3, num4, num5 };
             //int maxNum = nums.Max();
             //Console.WriteLine($"The maximum number is: {maxNum}");
 
-            //Task 5
+            ////Task 5
             //Console.Write("Enter kilometers per hour: ");
             //double km = Convert.ToDouble(Console.ReadLine());
             //double mile = km * 0.621371192;
             //Console.WriteLine($"{km} kilometers per hour is equal to {mile} miles per hour.");
 
-            //Task 6
+            ////Task 6
             //Console.Write("Enter hours: ");
             //int hours = Convert.ToInt32(Console.ReadLine());
             //Console.Write("Enter minutes: ");
@@ -66,34 +66,39 @@ namespace Task_2
             //int totalMinutes = hours * 60 + minutes;
             //Console.WriteLine($"Total: {totalMinutes} minutes.");
 
-            //Task 7
+            ////Task 7
             //Console.Write("Enter minutes: ");
-            //int totalMinutes = Convert.ToInt32(Console.ReadLine());
-            //int hours = totalMinutes / 60;
-            //int minutes = totalMinutes % 60;
+            //totalMinutes = Convert.ToInt32(Console.ReadLine());
+            //hours = totalMinutes / 60;
+            //minutes = totalMinutes % 60;
             //Console.WriteLine($"{hours} Hours, {minutes} Minutes");
 
-            //Task 8
-            string[] sentences = {
-                "BBQ sauce is the best invintion humans ever made",
-                "Hello",
-                "pink floyed is the greatest band ever",
-                "fried potato is awesome",
-                "ps is better than xbox"
-            };
-            Console.WriteLine(sentences[0]);
-            Console.WriteLine(sentences[1]);
-            Console.WriteLine(sentences[2]);
-            Console.WriteLine(sentences[3]);
-            Console.WriteLine(sentences[4]);
+            ////Task 8
+            //string[] sentences = {
+            //    "BBQ sauce is the best invintion humans ever made",
+            //    "Hello",
+            //    "pink floyed is the greatest band ever",
+            //    "fried potato is awesome",
+            //    "ps is better than xbox"
+            //};
+            //Console.WriteLine(sentences[0]);
+            //Console.WriteLine(sentences[1]);
+            //Console.WriteLine(sentences[2]);
+            //Console.WriteLine(sentences[3]);
+            //Console.WriteLine(sentences[4]);
 
-            Console.WriteLine(sentences[0].Length);
-            Console.WriteLine(sentences[1].Length);
-            Console.WriteLine(sentences[2].Length);
-            Console.WriteLine(sentences[3].Length);
-            Console.WriteLine(sentences[4].Length);
+            //Console.WriteLine(sentences[0].Length);
+            //Console.WriteLine(sentences[1].Length);
+            //Console.WriteLine(sentences[2].Length);
+            //Console.WriteLine(sentences[3].Length);
+            //Console.WriteLine(sentences[4].Length);
 
-
+            int i = 0;
+            while (i < 999999999)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
 
         }
     }
---
+++ 27.06.2024/task 3/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Q1
+            //int[] ARR = { 1, 7, 9, 45 };
+            //string[] arr2 = { "Str", "alex", "moh" };
+            //string[] arr3 = { "the", "fox", "over", "lazy", "dog" };
+
+            //Q2
+            //String[] fruits = { "Tomato", "Banana", "Watermelon" };
+            //int index1 = Array.IndexOf(fruits, "Banana");
+            //int index2 = Array.IndexOf(fruits, "Tomato");
+            //Console.WriteLine(index1);
+            //Console.WriteLine(index2);
+
+            //Q3
+            //string[] favoriteFood = { "burgers", "pizza", "fettuccine", "maqluba", "hotdogs" };
+            //string[] favoriteSports = { "basketball", "table tennis", "chess" };
+            //string[] favoriteMovies = { "Leon: The Professional", "Interstellar", "La La Land", "Marvel's" };
+
+            //Console.WriteLine("My favorite food:");
+            //foreach (string food in favoriteFood)
+            //{
+            //    Console.WriteLine(food);
+            //}
+            //Console.WriteLine("\nMy favorite sports:");
+            //foreach (string sport in favoriteSports)
+            //{
+            //    Console.WriteLine(sport);
+            //}
+            //Console.WriteLine("\nMy favorite movies:");
+            //foreach (string movie in favoriteMovies)
+            //{
+            //    Console.WriteLine(movie);
+            //}
+
+            //Q4
+            //Console.Write("Input three numbers separated by comma: ");
+            //string inputNums = Console.ReadLine();
+            //string[] numsString = inputNums.Split(',');
+            //int[] nums = Array.ConvertAll(numsString, int.Parse);
+            //int sum = nums[0] + nums[1] + nums[2];
+            //Console.WriteLine($"The sum of three numbers: {sum}");
+
+            //Q5
+            //Console.Write("Input the number of terms: ");
+            //int num = int.Parse(Console.ReadLine());
+            //int sum = 0;
+            //int count = 0;
+            //Console.WriteLine("The odd numbers are:");
+            //for (int i = 0; i <= 100 && count < num; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.Write(i + " ");
+            //        sum += i;
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine($"\nThe Sum of odd Numbers is: {sum}");
+
+            //Q6
+            //int rows = 4;
+            //for (int i = 1; i <= rows; i++)
+            //{
+            //    for (int g = rows; g > i; g--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= i; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+
+            //Q7
+            //int rows = 4;
+            //int num = 1;
+            //for (int i = 1; i <= rows; i++)
+            //{
+            //    for (int g = rows; g > i; g--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= i; k++)
+            //    {
+            //        Console.Write(num);
+            //        num++;
+            //    }
+            //    Console.WriteLine();
+            //}
+        }
+    }
+}
