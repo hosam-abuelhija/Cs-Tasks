011b7ce33f70f98cd523289f1cf322663fa79302
hosam-abuelhija <hosam.adnan123@gmail.com>
2024-07-03 18:25:12 +0300

    03.07.2024

---
+++ 03.07.2024/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var x = Console.ReadLine();
+            //Console.WriteLine(x);
+            //Console.WriteLine(x[x.Length - 1]);
+
+            //var x = Console.ReadLine();
+            //var y = Console.ReadLine();
+            //var z = string.Concat(x, " ", y);
+            //Console.WriteLine(z);
+
+        }
+    }
+}
